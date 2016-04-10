453e2b2434253165bec18a69a148e0fed27cbe27
Jose Miguel Garcia <jmgbenayas@gmail.com>
2016-04-10 19:02:06 +0200

    Nodo

--- practica-daa-ryp/src/Nodo.java
+++ practica-daa-ryp/src/Nodo.java
@@ -1,3 +1,20 @@
 public class Nodo {
-
+	final int N = 5;
+	private int nivel;
+	private int[] solucion = new int[N];
+	// matriz => Disponible en entrada.
+	private int cota; // Cota
+	
+	public Nodo nodoInicial(){
+		Nodo nodo = new Nodo();
+		for (int i = 0; i < solucion.length; i++) {
+			nodo.solucion[i] = 0;
+		}
+		nodo.nivel = 0;
+		/*
+		 * nodo.matriz[1,1] = ...
+		 */
+		return nodo;
+	}
+	
 }
