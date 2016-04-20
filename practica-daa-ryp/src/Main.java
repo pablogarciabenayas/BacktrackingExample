fdd93664e16514bc32cf7aa44d2e777b241ef2c4
Jose Miguel Garcia <jmgbenayas@gmail.com>
2016-04-20 21:40:47 +0200

    Main y Nodo nuevo

--- practica-daa-ryp/src/Main.java
+++ practica-daa-ryp/src/Main.java
@@ -1,15 +1,59 @@
+import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
-
-	private int cota=0;
-	private int[] solucion;	
 	
-//	ArrayList<Nodo> nodos = new ArrayList();
+	static int filas;
+	static int columnas;
+	static int[][] matriz_beneficios;
+	static int[] pedido;
+	static int cota = 0;
+	static int[] solucion;	
+	 
+	
+	
 	public static void main(String[] args) throws IOException {
+		
+		// INICIALIZAR FILAS, COLUMNAS, MATRIZ Y PEDIDO CON EL FICHERO ENTRADA!		
+	 
+		// CALCULO LA COTA INICIAL Y LA ASIGNACION INICIAL DE PASTELEROS
+		int cota_superior = 0;
+		int [] asignacion_inicial = new int[pedido.length];
+		for(int i = 0; i<pedido.length;i++){
+			asignacion_inicial[i] = matriz_beneficios[i][pedido[i]];
+		}
+		for(int i = 0; i < asignacion_inicial.length;i++){ 
+			cota_superior += asignacion_inicial[i];
+		}
+		solucion = asignacion_inicial;
+		
+		// CREO LA LISTA DE NODOS VIVOS
+		ArrayList<Nodo> nodos_vivos = new ArrayList<Nodo>();		
+		
+		// METEMOS LOS DATOS EN UN NUEVO NODO
+		Nodo nuevo_nodo = new Nodo(filas,columnas,matriz_beneficios,pedido);
+		
+		// CALCULO COTA PARA ESE NODO
+		nuevo_nodo.setCota();
+		
+		// AÑADO EL NODO A LA LISTA DE NODOS VIVOS
+		nodos_vivos.add(nuevo_nodo);
+		
+		while (!nodos_vivos.isEmpty()){
+			
+			Nodo padre = nodos_vivos.remove(0);
+			
+			ArrayList<Nodo> complecciones = getComplecciones(padre,filas,columnas);
+			
+			
+		}
+		
+		
+		
 //		if(args[0] == null){
 //			throw new IOException("Es necesario un fichero de entrada!!!");
 //		}
@@ -40,15 +84,8 @@ public class Main {
 			e.printStackTrace();
 		}	
 	}
-	
-	public int solucion_incial(int N, int[] pedido, int[][] matriz){
-		int [] beneficio = new int[N];
-		for(int i = 0; i<pedido.length;i++){
-			beneficio[i] = matriz[i][pedido[i]];
-		}
-		for(int i = 0; i<beneficio.length;i++){ 
-			cota += beneficio[i];
-		}
-		return cota;
+	public static ArrayList<Nodo> getComplecciones (Nodo padre, int n, int m){
+		ArrayList<Nodo> hijos = new ArrayList<Nodo> ();
+		
 	}
 }
